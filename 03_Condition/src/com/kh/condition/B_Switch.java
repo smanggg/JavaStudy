c915d3ea0a47f22c1151ecd48805080b1dc8a05b
smanggg <wjdthakd91@gmail.com>
2025-09-02 15:11:57 +0900

    Add files via upload

---
+++ 03_Condition/src/com/kh/condition/B_Switch.java
@@ -0,0 +1,59 @@
+package com.kh.condition;
+
+import java.util.Scanner;
+
+public class B_Switch {
+	/*
+	 * switch
+	 * : if문과 동일한 조건문이지만, 동등비교를 통해 흐름을 제어
+	 * : 실행할 코드만 실행하고 종료하기 위해선 break 를 사용!
+	 * 
+	 * switch (비교대상) {
+	 * 		case 값1:
+	 * 			// 비교대상 == 값1 경우 실행할 내용
+	 * 			break;
+	 * 		case 값2:
+	 * 			// 실행할 내용
+	 * 			[break;]  // 생략 가능. 단, 아내 내용이 이어서 실행됨!
+	 * 		default:
+	 * 			// 모든 case에 해당되지 않을 때
+	 * 			// 실행할 내용
+	 * 			[break;] 보안검사 시 써야할때도있다
+	 * }
+	 */
+	public static void main(String[] args) {
+		method1();
+		
+	}
+	
+	public static void method1() {
+		/* 정수를 입력받아 아래 조건에 맞게 출력
+		 * 
+		 * 1: 빨간색
+		 * 2: 노란색
+		 * 3: 파란색
+		 * 그외: 초록색
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("1 ~ 3 사이의 정수를 입력 : ");
+		int num = sc.nextInt();
+		
+		// --> 비교 대상 : num (입력받은 값)
+		switch(num) {
+			case 1:
+				System.out.println("빨간색");
+				break;
+			case 2:
+				System.out.println("노란색");
+				break;
+			case 3:
+				System.out.println("파란색");
+				break;
+			default:
+				System.out.println("초록색");
+		}
+	}
+	
+
+}
---
+++ 04_Loop/src/com/kh/loop/B_While.java
@@ -0,0 +1,32 @@
+package com.kh.loop;
+
+public class B_While {
+	/*
+	 * while 문
+	 * 
+	 * [표현식]
+	 * 		[초기식;] // => 생략 가능!
+	 * 		while(조건식) {
+	 * 			// 반복할 내용
+	 * 			[증감식;] => 생략 가능!
+	 * 		}
+	 * 	* 실행 순서
+	 * 		조선식 검사 -> 조건식 결과가 true인 경우 반복할 내용 수행
+	 * 		-> 조건식 검사 -> 조건식 결과가 true인 경우 반복할 내용 수행
+	 * 		---------- 계속 반복 -----------
+	 * 		-> 조건식 검사 -> 조건식 경과 false인 경우 반복문 종료
+	 */
+
+	public static void main(String[] args) {
+		
+		// "Happy" 5번 출력 -> while문 사용
+		int n = 10;
+		while(n >5) {
+			System.out.println("Happy");
+			
+			n--;
+		}
+
+	}
+
+}
